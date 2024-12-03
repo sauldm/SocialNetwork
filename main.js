3ed5b8b7e8bd970b219dd9de9896e0c147e3a0f0
Andres <anndrees31@gmail.com>
2024-12-03 09:35:09 +0100

    Created index for posts display

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-let prueba = "Hola";
-console.log(prueba);
-let prueba2 = "prueba2";
-console.log(prueba2);
+import {posts} from './data/posts.js';
+
+const container = document.getElementById('container');
+const template = document.getElementById('template').content;
+const fragment = document.createDocumentFragment();
+
+posts.forEach(post => {
+  const clone = template.cloneNode(true);
+  clone.querySelector('h2').textContent = post.title;
+  clone.querySelector('p').textContent = post.body;
+  fragment.appendChild(clone);
+});
+
+container.appendChild(fragment);
